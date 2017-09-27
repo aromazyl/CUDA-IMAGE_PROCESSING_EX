2034747ceb942ce0a07d0630122f3ba7c7a07b75
aromazyl <zyl2336709@gmail.com>
2017-09-27 21:01:11 +0800

    add run

--- possion_blending.cu
+++ possion_blending.cu
@@ -105,6 +105,7 @@ __global__ void compute_masks_kernel(const uchar4* d_sourceImg,
     }
   }
 }
+/*
 
 __global__ void separate_kernel(
     const uchar4* d_sourceImg,
@@ -124,6 +125,7 @@ __global__ void separate_kernel(
   g_channel[index] = rgba.y;
   b_channel[index] = rgba.z;
 }
+*/
 
 __global__ void ComputeSum2Kernel(const char* d_mask,
     const uchar4* d_sourceImg, uchar4* t_sum2) {
@@ -159,41 +161,86 @@ __global__ void JacobiKernel(
   int xmid = threadIdx.x + blockIdx.x * blockDim.x;
   int ymid = threadIdx.y + blockIdx.y * blockDim.y;
   if (xmid >= numCols || ymid >= numRows) return;
-  uchar4 sum1 = 0;
-  uchar4 neighbor;
+  int4 sum1 = 0;
   if (ymid > 0) {
-    int val = d_mask[xmid + (ymid - 1) * numCols];
+    uchar4 val = d_mask[xmid + (ymid - 1) * numCols];
     if (val != 0) {
       val = bufferA[xmid + (ymid - 1) * numCols];
-      sum1 += val;
+      sum1.x += val.x;
+      sum1.y += val.y;
+      sum1.z += val.z;
     }
   }
   if (ymid < numRows - 1) {
-    int val = d_mask[xmid + (ymid + 1) * numCols];
+    uchar4 val = d_mask[xmid + (ymid + 1) * numCols];
     if (val != 0) {
       val = bufferA[xmid + (ymid + 1)] * numCols;
-      sum1 += val;
+      sum1.x += val.x;
+      sum1.y += val.y;
+      sum1.z += val.z;
     }
   }
   if (xmid > 0) {
-    int val = d_mask[xmid - 1 + ymid * numCols];
+    uchar4 val = d_mask[xmid - 1 + ymid * numCols];
     if (val != 0) {
       val = bufferA[xmid - 1 + ymid * numCols];
-      sum1 += val;
+      sum1.x += val.x;
+      sum1.y += val.y;
+      sum1.z += val.z;
     }
   }
   if (xmid < numCols - 1) {
-    int val = d_mask[xmid + 1 + ymid * numCols];
+    uchar4 val = d_mask[xmid + 1 + ymid * numCols];
     if (val != 0) {
       val = bufferA[xmid + 1 + ymid * numCols];
-      sum1 += val;
+      sum1.x += val.x;
+      sum1.y += val.y;
+      sum1.z += val.z;
     }
   }
-  float4 newVal = (sum1 + sum2[xmid+ymid*numCols]) / 4;
+  float4 newVal;
+  newVal.x = (sum1.x + sum2[xmid+ymid*numCols].x) / 4;
+  newVal.y = (sum1.y + sum2[xmid+ymid*numCols].y) / 4;
+  newVal.z = (sum1.z + sum2[xmid+ymid*numCols].z) / 4;
 
   bufferB[xmid + ymid * numCols].x = min(255, max(0, int(newVal.x)));
   bufferB[xmid + ymid * numCols].y = min(255, max(0, int(newVal.y)));
   bufferB[xmid + ymid * numCols].z = min(255, max(0, int(newVal.z)));
+}
+
+__global__ void InitBuffer(
+    const uchar4* const d_sourceImg, uchar4* buffer,
+    const char* d_mask, const uchar4* const d_destImg,
+    size_t numRowsSource, size_t numColsSource) {
+  int xtid = threadIdx.x;
+  int xmid = threadIdx.x + blockDim.x * blockIdx.x;
+  int ytid = threadIdx.y;
+  int ymid = threadIdx.y + blockDim.y * blockIdx.y;
+  if (xmid >= numColsSource || ymid >= numRowsSource) return;
+  int pos = xmid + ymid * numColsSource;
+  if (d_mask[pos] == 1) {
+    buffer[pos] = d_sourceImg[pos];
+  } else if (d_mask[pos] == 2) {
+    buffer[pos] = d_destImg[pos];
+  } else {
+    buffer[pos] = 0;
+  }
+}
+
+__global__ void CopyResult(const uchar4* buffer, const char* d_mask,
+    uchar4* d_blendedImg, const uchar4* d_destImg,
+    const size_t numRowsSource, const size_t numColsSource) {
+  int xtid = threadIdx.x;
+  int xmid = threadIdx.x + blockDim.x * blockIdx.x;
+  int ytid = threadIdx.y;
+  int ymid = threadIdx.y + blockDim.y * blockIdx.y;
+  if (xmid >= numColsSource || ymid >= numRowsSource) return;
+  int pos = xmid + ymid * numColsSource;
+  if (d_mask[pos] == 1) {
+    d_blendedImg[pos] = buffer[pos];
+  } else {
+    d_blenedImg[pos] = d_destImg[pos];
+  }
 }
 
 void your_blend(const uchar4* const h_sourceImg,  //IN
@@ -201,6 +248,56 @@ void your_blend(const uchar4* const h_sourceImg,  //IN
                 const uchar4* const h_destImg, //IN
                 uchar4* const h_blendedImg) //OUT
 {
+    uchar4* d_sourceImg;
+    uchar4* d_destImg;
+    uchar4* d_mask;
+    uchar4* bufferA;
+    uchar4* bufferB;
+    uchar4* sum2;
+
+    unsigned int image_size = numColsSource * numRowsSource;
+    checkCudaErrors(cudaMalloc(&d_sourceImg, image_size * sizeof(uchar4)));
+    checkCudaErrors(cudaMalloc(&d_destImg, image_size * sizeof(uchar4)));
+    checkCudaErrors(cudaMalloc(&d_mask, image_size * sizeof(char)));
+    checkCudaErrors(cudaMalloc(&bufferA, image_size * sizeof(uchar4)));
+    checkCudaErrors(cudaMalloc(&bufferB, image_size * sizeof(uchar4)));
+    checkCudaErrors(cudaMalloc(&sum2, image_size * sizeof(uchar4)));
+
+    dim3 gridDim(numColsSource / 32 + 1, numRowsSource / 32 + 1);
+    dim3 blockDim(32, 32);
+    checkCudaErrors(cudaMemcpy(d_sourceImg, h_sourceImg, image_size * sizeof(uchar4)));
+    checkCudaErrors(cudaMemcpy(d_destImg, h_destImg, image_size * sizeof(uchar4)));
+
+    compute_masks_kernel<<<gridDim, blockDim>>>(d_sourceImg, numRowsSource, numColsSource, d_destImg, d_mask);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    ComputeSum2Kernel<<<gridDim, blockDim>>>(d_mask, d_sourceImg, sum2);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    InitBuffer<<<gridDim, blockDim>>>(d_sourceImg, bufferA, d_mask, d_destImg, numRowsSource, numColsSource);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    for (int turn = 0; turn < 800; ++turn) {
+      if (turn & 0x01) {
+        JacobiKernel<<<gridDim, blockDim>>>(d_mask, sum2, bufferB, bufferA, numColsSource, numRowsSource);
+      } else {
+        JacobiKernel<<<gridDim, blockDim>>>(d_mask, sum2, bufferA, bufferB, numColsSource, numRowsSource);
+      }
+      cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastErrors());
+    }
+
+    checkCudaErrors(cudaFree(bufferB));
+    checkCudaErrors(cudaFree(sum2));
+
+    CopyResult<<<gridDim, blockDim>>>(bufferA, d_mask, d_blendedImg, d_destImg, numRowsSource, numColsSource);
+    cudaDeviceSynchronize(); cudaGetLastError();
+
+    checkCudaErrors(cudaFree(bufferA));
+    checkCudaErrors(cudaFree(d_mask));
+    checkCudaErrors(cudaFree(d_destImg));
+
+    checkCudaErrors(cudaMemcpy(h_blendedImg, d_blendedImg, sizeof(uchar4) * image_size));
+    checkCudaErrors(cudaFree(d_blendedImg));
 
   /* To Recap here are the steps you need to implement
   
